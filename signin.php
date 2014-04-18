776a1201731e9c122ecbc5c237922ab245cea86e
Clement RICHARD <clement.richard443@gmail.com>
2014-04-18 08:48:13 +0200

    modif header-footer + signin

--- signin.php
+++ signin.php
@@ -29,12 +29,55 @@ if (isset($_POST["login"]) && isset($_POST["password"]))
     }
 }
 ?>
+        <style type="text/css">
+            body {
+              padding-top: 40px;
+              padding-bottom: 40px;
+              background-color: #eee;
+            }
+
+            .form-signin {
+              max-width: 330px;
+              padding: 15px;
+              margin: 0 auto;
+            }
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+              margin-bottom: 10px;
+            }
+            .form-signin .checkbox {
+              font-weight: normal;
+            }
+            .form-signin .form-control {
+              position: relative;
+              height: auto;
+              -webkit-box-sizing: border-box;
+                 -moz-box-sizing: border-box;
+                      box-sizing: border-box;
+              padding: 10px;
+              font-size: 16px;
+            }
+            .form-signin .form-control:focus {
+              z-index: 2;
+            }
+            .form-signin input[type="email"] {
+              margin-bottom: -1px;
+              border-bottom-right-radius: 0;
+              border-bottom-left-radius: 0;
+            }
+            .form-signin input[type="password"] {
+              margin-bottom: 10px;
+              border-top-left-radius: 0;
+              border-top-right-radius: 0;
+            }
+
+        </style>
     <div class="container">
-        <form class="form-signin" style="margin-top: 15%;" method="POST" action="signin.php">
+        <form class="form-signin" style="margin-top: 15%;" method="POST" action="signin.php" role="form">
             <h2 class="form-signin-heading">Connectez-vous</h2>
-            <input type="text" class="input-block-level" placeholder="Nom d'utilisateur" name="login" >
-            <input type="password" class="input-block-level" placeholder="Mot de passe" name="password" >
-            <button class="btn btn-large btn-primary" type="submit">Connexion</button>
+            <input type="text" class="form-control" placeholder="Nom d'utilisateur" name="login" >
+            <input type="password" class="form-control" placeholder="Mot de passe" name="password" >
+            <button class="btn btn-lg btn-primary  btn-block" type="submit">Connexion</button>
         </form>
     </div> <!-- /container -->
 <?php 
