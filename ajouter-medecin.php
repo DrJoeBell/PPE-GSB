d5bfd92ba0bb42c44259571897481da96403e974
Valentin Plissonneau <valentin.plissonneau@gmail.com>
2014-04-18 10:53:06 +0200

    modif ajouter medecin

--- ajouter-medecin.php
+++ ajouter-medecin.php
@@ -38,43 +38,13 @@
 
 ?>
 
-      <fieldset>
-        <label>Adresse</label>
-        <textarea id="textarea" style="width:40%" cols="30" rows="1" name="adresse" placeholder="Adresse"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <label>Ville</label>
-        <textarea id="textarea" style="width:40%" cols="30" rows="1" name="ville" placeholder="Ville"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <label>Code postal</label>
-        <textarea id="textarea" style="width:40%" cols="30" rows="1" name="cp" placeholder="xxxxx"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <label>Téléphone</label>
-        <textarea id="textarea" style="width:40%" cols="30" rows="1" name="tel" placeholder="xx xx xx xx xx"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <label>Département</label>
-        <textarea id="textarea" style="width:40%" cols="30" rows="1" name="departement" placeholder="xx"></textarea>
-      </fieldset>
-
-      <hr>
-
-      <button style="margin:auto; width:40%;" type="submit" class="btn span6">Enregistrer</button>
-    </form>
-
-
 <div class="row">
-  <div class="col-lg-9">
+  <div class="col-sm-3"></div>
+  <div class="col-sm-6">
     <div class="well bs-component">
-      <form class="form-horizontal">
+      <form class="form-horizontal" action="ajouter-medecin.php" method="POST">
         <fieldset>
-          <legend>Ajouter un practicien pour le commercial :&nbsp;<?= $_SESSION['nom'] .'&nbsp;'. $_SESSION['prenom'] ;?></legend>
+          <legend>Ajouter un practicien pour <?= $_SESSION['nom'] .'&nbsp;'. $_SESSION['prenom'] ;?></legend>
           <div class="form-group">
             <label for="nom" class="col-lg-2 control-label">Nom</label>
             <div class="col-lg-10">
@@ -112,76 +82,33 @@
           </div>
 
           <div class="form-group">
-            <label for="adresse" class="col-lg-2 control-label">Adresse</label>
+            <label for="ville" class="col-lg-2 control-label">Ville</label>
             <div class="col-lg-10">
-              <input type="text" class="form-control" id="adresse" placeholder="Adresse">
+              <input type="text" class="form-control" id="ville" placeholder="Ville">
             </div>
           </div>
 
           <div class="form-group">
-            <label for="adresse" class="col-lg-2 control-label">Adresse</label>
+            <label for="cp" class="col-lg-2 control-label">Code postal</label>
             <div class="col-lg-10">
-              <input type="text" class="form-control" id="adresse" placeholder="Adresse">
+              <input type="text" class="form-control" id="cp" placeholder="XXXXX">
             </div>
           </div>
 
           <div class="form-group">
-            <label for="adresse" class="col-lg-2 control-label">Adresse</label>
+            <label for="tel" class="col-lg-2 control-label">Téléphone</label>
             <div class="col-lg-10">
-              <input type="text" class="form-control" id="adresse" placeholder="Adresse">
+              <input type="text" class="form-control" id="tel" placeholder="xx xx xx xx xx">
             </div>
           </div>
 
           <div class="form-group">
-            <label for="adresse" class="col-lg-2 control-label">Adresse</label>
+            <label for="departement" class="col-lg-2 control-label">Département</label>
             <div class="col-lg-10">
-              <input type="text" class="form-control" id="adresse" placeholder="Adresse">
+              <input type="text" class="form-control" id="departement" placeholder="XX">
             </div>
           </div>
 
-          <div class="form-group">
-            <label class="col-lg-2 control-label">Radios</label>
-            <div class="col-lg-10">
-              <div class="radio">
-                <label>
-                  <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-                  Option one is this
-                </label>
-              </div>
-              <div class="radio">
-                <label>
-                  <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                  Option two can be something else
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="select" class="col-lg-2 control-label">Selects</label>
-            <div class="col-lg-10">
-              <select class="form-control" id="select">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-              <br>
-              <select multiple="" class="form-control">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-lg-10 col-lg-offset-2">
-              <button class="btn btn-default">Cancel</button>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </div>
         </fieldset>
       </form>
     </div>
