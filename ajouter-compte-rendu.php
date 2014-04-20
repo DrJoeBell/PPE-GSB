55901921a308587a59aced116615a54a08fb696e
Mathieu Pasco <mathieu.pasco@gmail.com>
2014-04-20 10:27:58 +0200

    modification des requetes SQL pour les trier par ordre alphabétique

--- ajouter-compte-rendu.php
+++ ajouter-compte-rendu.php
@@ -22,11 +22,11 @@
     $bdd->query($query_insert);
   }
 
-  $query= "SELECT * FROM medecin ;";
+  $query= "SELECT * FROM medecin ORDER BY nom;";
   $result_medecin = $bdd->query($query);
-  $query= "SELECT * FROM motif ;";
+  $query= "SELECT * FROM motif ORDER BY libelle;";
   $result_motif = $bdd->query($query);
-  $query= "SELECT * FROM visiteur ;";
+  $query= "SELECT * FROM visiteur ORDER BY nom;";
   $result_visiteur = $bdd->query($query);
 
 
